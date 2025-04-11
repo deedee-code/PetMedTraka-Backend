13018f4d19e1a0d66ba4d9e5ce2332ab97da0fc4
Deedee <deedeeowoeye@gmail.com>
2025-04-11 10:02:43 +0100

    chore: add user role to user table

--- pet-med-traka/src/database/schemas/user.schema.ts
+++ pet-med-traka/src/database/schemas/user.schema.ts
@@ -1,6 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export enum UserRole {
+  Owner = 'Owner',
+  Veterinarian = 'Veterinarian',
+  Admin = 'Admin',
+}
+
 @Schema({ timestamps: true })
 export class User extends Document {
   @Prop({ required: true })
@@ -12,8 +18,8 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ default: 'user' })
-  role: string;
+  @Prop({ type: String, enum: UserRole, default: UserRole.Owner })
+  role: UserRole;
 
   @Prop({ default: true })
   isActive: boolean;
